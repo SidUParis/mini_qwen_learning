55471b41f2ed14525a0139b7b807f30faded7e92
Xu Sun <ddbq0338@yd-deskin-5.rd.francetelecom.fr>
2025-02-12 15:05:18 +0100

    update demo_data_veiw

--- utils/demo_view_data.py
+++ utils/demo_view_data.py
@@ -1,10 +1,25 @@
 from datasets import load_dataset
+import argparse 
+parser = argparse.ArgumentParser()
 
-dataset = load_dataset("parquet", data_files="mini_data/pt/accommodation_catering_hotel/chinese/high/rank_00000.parquet", split="train")
-print(dataset[0])
+parser.add_argument("type",help="please type which kinds of data you want to see",type=str,choices=['dpo','pt','sft'])
+args = parser.parse_args()
 
-dataset = load_dataset("parquet", data_files="mini_data/sft/7M/train-00000-of-00075.parquet", split="train")
-print(dataset[9])
+if args.type == "dpo":
+    dataset = load_dataset("parquet", data_files="mini_data/dpo/train-00000-of-00001.parquet", split="train")
+    print(dataset[5])
+elif args.type == 'sft':
+    dataset = load_dataset("parquet", data_files="mini_data/sft/7M/train-00000-of-00075.parquet", split="train")
+    print(dataset[9])
+elif args.type == 'pt':
+    dataset = load_dataset("parquet", data_files="mini_data/pt/accommodation_catering_hotel/chinese/high/rank_00000.parquet", split="train")
+    print(dataset[0])
 
-dataset = load_dataset("parquet", data_files="mini_data/dpo/train-00000-of-00001.parquet", split="train")
-print(dataset[5])
+
+
+
+
+
+
+
+
